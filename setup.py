fc172f2f9f33c3f837afb8d6be31c8b10eefc56a
Richard To <gohn76@gmail.com>
2012-07-25 08:18:24 -0800

    Added subpackage to setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     version='0.1',
     url='https://github.com/richard-to/dashgourd',
     author='Richard To',
-    packages=['dashgourd'],
-    namespace_packages=['dashgourd'],
+    packages=['dashgourd', 'dashgourd.api', 'dashgourd.importer'],
     install_requires=[
         'pymongo',
         'MySQL-python'
