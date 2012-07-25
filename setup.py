d7ef616138e7978e421d7505ab41e7f5e37ef5a4
Richard To <gohn76@gmail.com>
2012-07-25 08:08:54 -0800

    Another edit to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     url='https://github.com/richard-to/dashgourd',
     author='Richard To',
     packages=['dashgourd'],
+    namespace_packages=['dashgourd'],
     install_requires=[
         'pymongo',
         'MySQL-python'
