9bc08ebc885fa86865358c6a1bb08989d2c1f50b
Richard To <gohn76@gmail.com>
2012-07-25 08:23:29 -0800

    Adding missing package to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ setup(
     url='https://github.com/richard-to/dashgourd',
     author='Richard To',
     platforms='any',
-    packages=['dashgourd', 'dashgourd.api', 'dashgourd.importer'],
+    packages=[
+        'dashgourd', 
+        'dashgourd.api', 
+        'dashgourd.importer', 
+        'dashgourd.plugins'
+    ],
     include_package_data=True,
     install_requires=[
         'pymongo',
