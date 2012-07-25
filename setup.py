28932147f2827e84a2ea2a03716c6f023d962d6e
Richard To <gohn76@gmail.com>
2012-07-25 08:21:23 -0800

    Still working with setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='0.1',
     url='https://github.com/richard-to/dashgourd',
     author='Richard To',
+    platforms='any',
     packages=['dashgourd', 'dashgourd.api', 'dashgourd.importer'],
+    include_package_data=True,
     install_requires=[
         'pymongo',
         'MySQL-python'
