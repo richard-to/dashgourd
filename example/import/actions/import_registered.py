1cc7f1a42dd1a2341047cd3a95e927e33fb0ee3e
Richard To <gohn76@gmail.com>
2012-08-03 15:09:55 -0800

    Removed examples. They keep getting out of date.

--- example/import/actions/import_registered.py
+++
@@ -1,8 +0,0 @@
-from dashgourd.importer.mysql import MysqlImportHelper
-
-importer = MysqlImportHelper()
-query = """
-    select id _id, created_at from user
-    """
-importer.import_actions('registered', query)
-importer.close()
--- example/import/import_user.py
+++
@@ -1,8 +0,0 @@
-from dashgourd.importer.mysql import MysqlImportHelper
-
-importer = MysqlImportHelper()
-query = """
-    select id _id, referrer, birthday, gender, created_at from user
-    """
-importer.import_users(query)
-importer.close()
