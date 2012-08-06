6025ebeb239854a53ef915d9fe0893a63a6c86a3
Richard To <gohn76@gmail.com>
2012-08-05 21:51:38 -0800

    Added option in actions api to allow only unique actions to be inserted

--- dashgourd/api/actions.py
+++ dashgourd/api/actions.py
@@ -1,3 +1,4 @@
+import copy
 from dashgourd.api.helper import init_mongodb
 
 class ActionsApi(object):
@@ -38,31 +39,36 @@ class ActionsApi(object):
             data['ab'] = {}
             self.db.users.insert(data)
                        
-    def insert_action(self, user_id, data):
+    def insert_action(self, user_id, data, unique=False):
         """Logs a user action
         
-        Actions that have not been logged in the db
-        are stored in an action collection.
-        
-        `user_id` is used to find the user that did the action.
+        `user_id` is used to find the user that 
+        performed/received the action.
         
         `type` is basically the slug name of the action.
         
         See create_user method for details on why created_at fields 
         should be converted to UTC beforehand.
         
-        Example: `user_registered` would become `User Registered`
-        
         `created_at` is the date the action ocurred.
-        
         
         Args:
             user_id: Id of user
             data: Dict that contains at least `name`, and `created_at` keys
+            unique: Actions will only be inserted if unique 
         """        
         
         if ('name' in data and 
-            'created_at' in data):            
+            'created_at' in data):
+
+            if unique is True:
+                insert_values = copy.copy(data)
+                del insert_values['created_at']
+                user = self.db.users.find_one({ 
+                    'user_id':user_id,
+                    'actions':{'$elemMatch': insert_values}}, {"user_id": 1})
+                if user:
+                    return
             self.db.users.update({ 'user_id':user_id }, { '$push': { 'actions': data } })
     
     def tag_abtest(self, user_id, data): 
