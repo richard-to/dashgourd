ea7d8da96ba1b2874e8fa6267620c471aaddc834
Richard To <gohn76@gmail.com>
2012-08-26 21:50:26 -0800

    Also remove comments from this file

--- dashgourd/api/charts.py
+++ dashgourd/api/charts.py
@@ -4,18 +4,8 @@ from dashgourd.charts.retention import Retention
 from dashgourd.charts.action_cohort import ActionCohort
 
 class ChartsApi(object):
-    """Api for charts.
     
-    Api allows for creation and fetching of charts.
-        
-    Attributes:
-        mongodb: MongoDb db or Mongodb connection string
-        dbname: If passing in connection string, dbname is needed
-        plugins: Dict of chart plugins to load. If None, then defaults used
-    """
-    
-    def __init__(self, mongodb, dbname=None, plugins=None):
-        
+    def __init__(self, mongodb, dbname=None, plugins=None):     
         if type(mongodb) is str:
             self.db = init_mongodb(mongodb, dbname)
         else:
@@ -30,47 +20,12 @@ class ChartsApi(object):
         else:
             self.plugins = plugins
       
-    def get_chart(self, collection):      
-        """Gets chart from DashGourd
-        
-        Gets chart data based on collection name.
-        
-        Data will not be returned in sorted order.
-        
-        Args:
-            collection: Name of chart
-        
-        Returns: 
-            PyMongo cursor of collection
-            
-        TODO(richard-to): Check if valid chart collection
-        TODO(richard-to): Returning pymongo cursor probably not the best idea.
-        """  
-          
+
+    def get_chart(self, collection):
         return self.db[collection].find()
     
+
     def generate_chart(self, plugin, collection, options):
-        """Generates chart based on plugin.
-        
-        This method is basically a wrapper for plugins.
-        Plugin functions can be used directly.
-        
-        Does not check what options are passed into plugin.
-        Plugin needs to check that expected values are passed in.
-        
-        Currently errors handled silently.
-        
-        Args:
-            plugin: Name of plugin
-            collection: Name of collection
-            options: Dict of options needed by plugin
-        
-        Returns:
-            Chart plugin for generating chart data. Or returns False.
-            
-        TODO(richard-to): How should errors be handled?
-        """
-        
         plugin = self.plugins.get(plugin)
         if plugin is not None:
             return plugin.run(self.db, collection, options)
