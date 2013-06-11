dba3d1b4a127562cdfdbdeb4b72074ab3feeaf88
richard-to <gohn76@gmail.com>
2013-06-10 23:52:31 -0800

    Fix bug to handle empty mysql password

--- dashgourd/api/imports.py
+++ dashgourd/api/imports.py
@@ -3,17 +3,17 @@ from dashgourd.api.helper import init_mongodb
 
 class ImportApi(object):
     def __init__(self, mongodb, dbname=None):
-        
+
         if type(mongodb) is str:
             self.db = init_mongodb(mongodb, dbname)
         else:
             self.db = mongodb
-    
-    
-    def get_last_update(self, query_name):        
+
+
+    def get_last_update(self, query_name):
         result = None
         result = self.db.import_logs.find_one({"_id": query_name})
-    
+
         if result is not None:
             return result['last_update']
         else:
@@ -21,11 +21,10 @@ class ImportApi(object):
             self.db.import_logs.insert(
                 {"_id": query_name, 'last_update': import_settings['start_date']})
             return import_settings['start_date']
-        
+
 
     def set_last_update(self, query_name, last_update):
         self.db.import_logs.update(
-            {"_id": query_name}, 
-            {'last_update': last_update}, 
+            {"_id": query_name},
+            {'last_update': last_update},
             True)
-        
